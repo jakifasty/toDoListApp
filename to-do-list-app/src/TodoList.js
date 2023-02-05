0789ede820fe62ba2cf2678a5edb0adfa6bb9c7a
Borja Javierre <borja.general@gmail.com>
2023-02-05 17:02:07 +0100

    add and fix name TodoList.js file

---
+++ to-do-list-app/src/TodoList.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import ToDo from './Todo'
+
+export default function ToDoList({todos}) {
+
+    function setTodos(){
+
+    }
+
+    return (
+        todos.map(todo=> {
+            return <ToDo key={todo.id} todo={todo.name}/>
+        })
+    )
+}
