aa2b77f21b6a7726e53fbd08f52eaf35d5eb30fa
Borja Javierre <borja.general@gmail.com>
2023-02-05 13:52:15 +0100

    initial app adn d styles file

---
+++ to-do-list-app/src/App.js
@@ -0,0 +1,36 @@
+import './style.css'
+
+function App() {
+  return (
+    <div className="grid-calculator">
+      <div className="output">
+        <div className="previous-operand"></div>
+        <div className="current-operand"></div>
+      </div>
+      <button className="span-two">AC</button>
+      <button className="span-one">DEL</button>
+      <button className="span-one">/</button>
+
+      <button className="span-one">1</button>
+      <button className="span-one">2</button>
+      <button className="span-one">3</button>
+      <button className="span-one">*</button>
+
+      <button className="span-one">4</button>
+      <button className="span-one">5</button>
+      <button className="span-one">6</button>
+      <button className="span-one">+</button>
+
+      <button className="span-one">7</button>
+      <button className="span-one">8</button>
+      <button className="span-one">9</button>
+      <button className="span-one">-</button>
+
+      <button className="span-one">.</button>
+      <button className="span-one">0</button>
+      <button className="span-two">=</button>
+    </div>
+  );
+}
+
+export default App;
